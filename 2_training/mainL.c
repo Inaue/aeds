59d9c6a5c77d7e9f72e6022dda5642ecbffb314c
Inaue <cafe.ina@outlook.com>
2022-06-02 12:25:16 +0000

    implementing lines and lists for other exercises

---
+++ 2_training/mainL.c
@@ -0,0 +1,38 @@
+/**
+ * @file   mainL.c
+ * @brief  RESOLUCAO DOS EXERCICIOS DE LISTAS
+ * @author Inaue Ferreira da Silva
+ * @date   02-06-2022
+ */
+
+/*  CABECALHOS  */
+#include <stdio.h>
+
+/*  CODIGOS DE ERRO */
+#define EXECUTADO_COM_EXITO     0
+
+/*  CONSTANTES  */
+
+#define VERDADEIRO  (1 == 1)
+#define FALSO       !VERDADEIRO
+
+/*  FUNCOES     */
+/*  *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   */
+int     main	                (int argc, char** argv);
+/*void	exec1			(void);*/
+
+/*  *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   */
+
+/**
+ * @brief FUNCAO PRINCIPAL
+ *
+ * @param argc NUMERO DE ARGUMENTOS
+ * @param argv ARGUMENTOS
+ * @return int CODIGO DE ERRO
+ */
+int main(int argc, char** argv)
+{
+    /*exec1();*/
+
+    return EXECUTADO_COM_EXITO;
+}
