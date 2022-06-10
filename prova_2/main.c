72ed103a5242f4fff574485c85d14af7fa71392f
Inaue <cafe.ina@outlook.com>
2022-06-10 14:41:57 +0000

    ordering the priority queue and printing answer

--- prova_2/main.c
+++ prova_2/main.c
@@ -7,6 +7,7 @@
 
 /*  CABECALHOS  */
 #include <stdio.h>
+#include <stdlib.h>
 #include "pilhas.h"
 #include "fila2.h"
 
@@ -20,38 +21,63 @@
 /*  FUNCOES     */
 /*  *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   */
 
-int         main		(int argc, char** argv);
-void        interface_problema 	(void);
+int	main			(int argc, char** argv);
+void	interface_problema 	(void);
+void	fila_insere_ordenado_l	(FilaL2 *l, int v);
 
 /*  *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   */
+
+void fila_insere_ordenado_l(FilaL2 *l, int v){
+	Lista *novo;
+	novo=(Lista *)malloc(sizeof(Lista));
+	novo->info=v;
+	Lista *ant=NULL;
+	Lista *p=l->ini;
+	while(p!=NULL && p->info>v){
+		ant=p;
+		p=p->prox;
+	}
+	if(ant==NULL){
+		novo->prox=l->ini;
+		l->ini=novo;
+	}
+	else{
+		ant->prox=novo;
+		novo->prox=p;
+	}
+}
 
 /**
  * @brief INTERFACE DO USUARIO
  */
 void interface_problema(void)
 {
-	int instrucoes, i, inst, num, saidaL, saidaP, saidaF, saidaFpri; 
+	int instrucoes, i, inst, num, saidaL, saidaP, saidaF, saidaFpri;
+	int ehPilha, ehFila, ehFilaP;
 	Pilha_vet* testeP;
 	Fila2* testeF;
 	FilaL2* testeFpri;
 
 	while(VERDADEIRO)
 	{
-		testeP = pilha_vet_cria();
-		testeF = fila_cria_vet();
-		testeFpri = fila_cria_l();
+		ehPilha		= VERDADEIRO;
+		ehFila		= VERDADEIRO;
+		ehFilaP		= VERDADEIRO;
+		testeP		= pilha_vet_cria();
+		testeF		= fila_cria_vet();
+		testeFpri	= fila_cria_l();
 
-		scanf("%i", &instrucoes);
+		scanf(" %i", &instrucoes);
 
 		for (i = 0; i < instrucoes; i++)
 		{
-			scanf("%i %i", &inst, &num);
+			scanf(" %i %i", &inst, &num);
 
 			if (inst == 1)
 			{
 				pilha_vet_push(testeP, num);
 				fila_insere_vet(testeF, num);
-				fila_insere_l(testeFpri, num);
+				fila_insere_ordenado_l(testeFpri, num);
 			}
 			else
 			{
@@ -59,11 +85,24 @@ void interface_problema(void)
 				saidaF = fila_retira_vet(testeF);
 				saidaFpri = fila_retira_l(testeFpri);
 
-				printf("%i\n", saidaP);
-				printf("%i\n", saidaF);
-				printf("%i\n", saidaFpri);
+				ehPilha	= ehPilha	&& (saidaP == num);
+				ehFila	= ehFila	&& (saidaF == num);
+				ehFilaP	= ehFilaP	&& (saidaFpri == num);
 			}
 		}
+
+		if (	(ehPilha && ehFila) ||
+			(ehPilha && ehFilaP) ||
+			(ehFila && ehFilaP)	)
+			printf("not sure\n");
+		else if (ehPilha)
+			printf("stack\n");
+		else if (ehFila)
+			printf("queue\n");
+		else if (ehFilaP)
+			printf("priority queue\n");
+		else
+			printf("impossible\n");
 
 		pilha_vet_libera(testeP);
 		fila_libera_vet(testeF);
