d317cb1cdaaf2b5abf88aafa111657a3d8abe4b5
Inaue <cafe.ina@outlook.com>
2022-06-21 23:17:04 +0000

    creating function for histogram; user interface

--- arvore/main.c
+++ arvore/main.c
@@ -74,23 +74,55 @@ int arv_cont_folha(Arv* Contar)
 	return (arv_cont_folha(Contar->esq) + arv_cont_folha(Contar->dir));
 }
 
+void arv_frequencia_c(Arv* Analisar, int* histograma)
+{
+	int c;
+
+	if (arv_vazia(Analisar))
+		return;
+
+	for (c = 0; c < 26; c++)
+	{
+		if (Analisar->info == ('a' + c))
+		{
+			histograma[c]++;
+			break;
+		}
+	}
+
+	arv_frequencia_c(Analisar->esq, histograma);
+	arv_frequencia_c(Analisar->dir, histograma);
+}
+
 void aula()
 {
 	Arv* avri = arv_criavazia();
+	int c, meuHistograma[26] = { 0 };
 
 	avri = arv_insere_ordenado(avri, 'c');
 	avri = arv_insere_ordenado(avri, 'a');
 	avri = arv_insere_ordenado(avri, 'b');
 	avri = arv_insere_ordenado(avri, 'd');
-	printf("Total de Folha: %i\n", arv_cont_folha(avri));
-	printf("Total de Galhos com 1 filho: %i\n", arv_cont_galho1(avri));
-	printf("Total de Galhos com 2 filhos: %i\n", arv_cont_galho2(avri));
+	avri = arv_insere_ordenado(avri, 'a');
+	avri = arv_insere_ordenado(avri, 'b');
+	arv_frequencia_c(avri, meuHistograma);
+	printf("_________________________________________________\n");
+
+	for (c = 0; c < 26; c++)
+		printf("Aparicoes da letra '%c': %i\n", ('a' + c), meuHistograma[c]);
+
+	printf("_________________________________________________\n");
+	printf("Total de Folha: \t\t%i\n", arv_cont_folha(avri));
+	printf("Total de Galhos com 1 filho: \t%i\n", arv_cont_galho1(avri));
+	printf("Total de Galhos com 2 filhos: \t%i\n", arv_cont_galho2(avri));
+	printf("_________________________________________________\n");
 	printf("Pre-ordem:\n");
 	arv_imprime_preordem(avri);
 	printf("\nSimetria:\n");
 	arv_imprime_simetrica(avri);
 	printf("\nPos-ordem:\n");
 	arv_imprime_posordem(avri);
+	printf("\n_________________________________________________\n");
 }
 
 /**
