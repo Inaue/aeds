ba1505577a203ff02e3ead6a4a33b3273451e569
Inaue <cafe.ina@outlook.com>
2022-07-07 00:37:58 +0000

    Starting to solve exercises related to graphs.

---
+++ grafo/ex1.c
@@ -0,0 +1,33 @@
+/**
+ * @file   ex1.c
+ * @brief  RESOLUCAO DO EXERCICIO DE GRAFOS
+ * @author Inaue Ferreira da Silva
+ * @date   07-06-2022
+ */
+
+/*  CABECALHOS  */
+#include <stdio.h>
+#include <stdlib.h>
+
+/*  CODIGOS DE ERRO */
+#define EXECUTADO_COM_EXITO     0
+
+/*  FUNCOES     */
+/*  *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   */
+
+int	main			(int argc, char** argv);
+
+/*  *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   */
+
+/**
+ * @brief FUNCAO PRINCIPAL
+ *
+ * @param argc NUMERO DE ARGUMENTOS
+ * @param argv ARGUMENTOS
+ * @return int CODIGO DE ERRO
+ */
+int main(int argc, char** argv)
+{
+
+    return EXECUTADO_COM_EXITO;
+}
