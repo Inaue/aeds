0f78c30a7e1b55912fec3e18fa22b671a1023e6d
Inaue <cafe.ina@outlook.com>
2022-08-13 14:37:19 +0000

    implementing function for BFS

--- grafo/busca_em_grafos/main.c
+++ grafo/busca_em_grafos/main.c
@@ -17,10 +17,13 @@
 /*	CONSTANTES	*/
 #define VERDADEIRO	(1 == 1)
 #define FALSO		!VERDADEIRO
+#define V_EXPLORANDO	0
+#define VIZINHOS_EXPLOR	1
+#define COMECO_BUSCA	0
+#define FIM_BUSCA	1
 #define NAO_PREENCHIDO	-1
 #define V_EXPLORAR	NAO_PREENCHIDO
-#define V_EXPLORADO	1
-#define VIZI_EXPLORADOS	2
+#define SEM_PREDECESSOR	NAO_PREENCHIDO
 
 /*	ESTRUTURAS	*/
 struct Info_bfs {
@@ -41,38 +44,38 @@ typedef struct Info_dfs Info_dfs;
 /*  *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   */
 
 int	main			(int argc, char** argv);
-void	grafo_bfs		(int** grafo, int total_vertices, int vertice_inicial, Info_bfs* guardar_resultado);
-void	grafo_dfs		(int** grafo, int total_vertices, int vertice_inicial, Info_dfs* guardar_resultado);
+void	grafo_bfs		(Lista** grafo, int total_vertices, int v_inicial, Info_bfs* resultado);
+void	grafo_dfs		(Lista** grafo, int total_vertices, int v_inicial, Info_dfs* resultado);
 void	lst_print		(Lista* lst_imprimir);
 
 /*  *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   *   */
 /*
-void	grafo_bfs		(int** grafo, int total_vertices, int vertice_inicial, int** guardar_resultado)
+void	grafo_bfs		(int** grafo, int total_vertices, int v_inicial, int** resultado)
 {
 	int predecessor, v;
 
-	guardar_resultado[ESTADO][vertice_inicial] = V_EXPLORADO;
+	resultado[ESTADO][v_inicial] = V_EXPLORANDO;
 	
-	if (guardar_resultado[PREDECESSOR][vertice_inicial] == NAO_PREENCHIDO)
+	if (resultado[PREDECESSOR][v_inicial] == NAO_PREENCHIDO)
 	{
-		guardar_resultado[DISTANCIA][vertice_inicial] = 0;
+		resultado[DISTANCIA][v_inicial] = 0;
 	}
 	else
 	{
-		predecessor = guardar_resultado[PREDECESSOR][vertice_inicial];
-		guardar_resultado[DISTANCIA][vertice_inicial] = guardar_resultado[DISTANCIA][predecessor] + 1;	
+		predecessor = resultado[PREDECESSOR][v_inicial];
+		resultado[DISTANCIA][v_inicial] = resultado[DISTANCIA][predecessor] + 1;	
 	}
 
 	for (v = 0; v < total_vertices; v++)
 	{
-		if ((grafo[vertice_inicial][v] == 1) && (guardar_resultado[ESTADO][v] == V_EXPLORAR))
+		if ((grafo[v_inicial][v] == 1) && (resultado[ESTADO][v] == V_EXPLORAR))
 		{
-			guardar_resultado[PREDECESSOR][v] = vertice_inicial;
-			grafo_bfs(grafo, total_vertices, v, guardar_resultado);
+			resultado[PREDECESSOR][v] = v_inicial;
+			grafo_bfs(grafo, total_vertices, v, resultado);
 		}
 	}
 
-	guardar_resultado[ESTADO][vertice_inicial] = VIZI_EXPLORADOS;
+	resultado[ESTADO][v_inicial] = VIZI_EXPLORADOS;
 }
 */
 
@@ -90,13 +93,35 @@ void	lst_print	(Lista* lst_imprimir)
 
 }
 
-void	grafo_bfs		(int** grafo, int total_vertices, int vertice_inicial, Info_bfs* guardar_resultado)
+void	grafo_bfs		(Lista** grafo, int total_vertices, int v_inicial, Info_bfs* resultado)
 {
-	FilaL* vertices_explorar = fila_cria_l();
+	int v_explorando;
+	FilaL* vertices_explorando = fila_cria_l();
+	Lista* v_adjacente;
 
-	fila_insere_l(vertices_explorar, vertice_inicial);
+	fila_insere_l(vertices_explorando, v_inicial);
+	resultado[v_inicial].estado		= V_EXPLORANDO; 
+	resultado[v_inicial].predecessor	= SEM_PREDECESSOR;
+	resultado[v_inicial].distancia		= 0;
 
-	fila_libera_l(vertices_explorar);
+	do {
+		v_explorando = fila_retira_l(vertices_explorando);
+
+		for (v_adjacente = grafo[v_explorando]; v_adjacente != NULL; v_adjacente = v_adjacente->prox)
+		{
+			if (resultado[v_adjacente->info].estado == V_EXPLORAR)
+			{
+				resultado[v_adjacente->info].predecessor	= v_explorando;
+				resultado[v_adjacente->info].estado		= V_EXPLORANDO;
+				resultado[v_adjacente->info].distancia		= resultado[v_explorando].distancia + 1;
+				fila_insere_l(vertices_explorando, v_adjacente->info);
+			}
+		}
+
+		resultado[v_explorando].estado = VIZINHOS_EXPLOR;
+	} while (!fila_vazia_l(vertices_explorando));
+
+	fila_libera_l(vertices_explorando);
 }
 
 /**
@@ -109,9 +134,9 @@ void	grafo_bfs		(int** grafo, int total_vertices, int vertice_inicial, Info_bfs*
 int main(int argc, char** argv)
 {
 	Lista** grafo;
-	Info_dfs* resultado_busca_dfs;
 	Info_bfs* resultado_busca_bfs;
-	int vertices, v, v1, v2, i, seed;
+	Info_dfs* resultado_busca_dfs;
+	int vertices, v, v1, v2, origem, i, seed;
 
 	printf("GRAFO ORIENTADO\n");
 	printf("____________________________________________________________\n");
@@ -119,12 +144,20 @@ int main(int argc, char** argv)
 	scanf("%i", &vertices);
 	printf("Digite um numero inteiro qualquer:\n");
 	scanf("%i", &seed);
+	printf("Digite a origem da busca:\n");
+	scanf("%i", &origem);
 	srand(seed);
-
 	grafo = (Lista**)malloc(vertices * sizeof(Lista*));
+	resultado_busca_bfs = (Info_bfs*)malloc(vertices * sizeof(Info_bfs));
+	resultado_busca_dfs = (Info_dfs*)malloc(vertices * sizeof(Info_dfs));
 
 	for (v = 0; v < vertices; v++)
+	{
 		grafo[v] = lst_cria();
+		resultado_busca_bfs[v].estado		= NAO_PREENCHIDO;
+		resultado_busca_bfs[v].predecessor	= NAO_PREENCHIDO;
+		resultado_busca_bfs[v].distancia	= NAO_PREENCHIDO;
+	}
 
 	for (v1 = 0; v1 < vertices; v1++)
 	{
@@ -144,7 +177,16 @@ int main(int argc, char** argv)
 		lst_print(grafo[v]);
 	}
 
+	printf("____________________________________________________________\n");
+	printf("vertice\tdistancia\tpredecessor\testado\n");
+	grafo_bfs(grafo, vertices, origem, resultado_busca_bfs);
+
+	for (v = 0; v < vertices; v++)
+		printf("%i\t%i\t\t%i\t\t%i\n", v, resultado_busca_bfs[v].distancia, resultado_busca_bfs[v].predecessor, resultado_busca_bfs[v].estado);
+
 	free(grafo);
+	free(resultado_busca_bfs);
+	free(resultado_busca_dfs);
 
 	return EXECUTADO_COM_EXITO;
 }
