c88c49e5e59907b9b08403bda1c6c59dfd31ff52
Inaue <cafe.ina@outlook.com>
2022-09-16 16:13:09 +0000

    Programming my first Java application.

---
+++ introducao_java/MeuPrograma.java
@@ -0,0 +1,7 @@
+class MeuPrograma {
+	public static void main(String[] args) {
+		System.out.println("Testando primeiro programa em java ...");
+		System.out.println("Funcionou!");
+		System.out.println("Imprimindo duas linhas com\napenas uma linha de codigo.");
+	}
+}
