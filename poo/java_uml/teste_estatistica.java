6081fdd9384172b4b9f4fc3e7818466e6b323567
Inaue <cafe.ina@outlook.com>
2022-10-10 14:56:28 +0000

    calculus of median

--- poo/java_uml/teste_estatistica.java
+++ poo/java_uml/teste_estatistica.java
@@ -14,7 +14,7 @@ class Teste_Estatistica {
 		amostra[8] = 0.77f;
 		amostra[9] = 0.10f;
 
-		System.out.println(Resultados.moda(amostra));
+		System.out.println(Resultados.mediana(amostra));
 	}
 }
 
@@ -24,11 +24,40 @@ class Estatistica {
 	}
 
 	public float media(float[] dados) {
-		return 0.0f;
+		float soma = 0.0f;
+
+		for(float d : dados)
+			soma += d;
+
+		return (soma / dados.length);
 	}
 
 	public float mediana(float[] dados) {
-		return 0.0f;
+		float[] ordem_crescente = new float[dados.length];
+		float aux;
+		int i = 0, j, centro;
+
+		for(float d : dados)
+			ordem_crescente[i++] = d;
+
+		for(i = 0; i < ordem_crescente.length; i++)
+		{
+			for(j = ordem_crescente.length-1; j > i; j--)
+			{
+				if(ordem_crescente[j] < ordem_crescente[j-1])
+				{
+					aux = ordem_crescente[j];
+					ordem_crescente[j] = ordem_crescente[j-1];
+					ordem_crescente[j-1] = aux;
+				}
+			}
+		}
+
+		centro = (ordem_crescente.length / 2);
+		if((ordem_crescente.length % 2) == 0)
+			return ((ordem_crescente[centro] + ordem_crescente[centro+1]) / 2);
+		else
+			return ordem_crescente[centro];
 	}
 
 }
