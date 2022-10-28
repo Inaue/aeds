56ccdfa2b29f550c9a4572996f31916a15b90063
Inaue <cafe.ina@outlook.com>
2022-10-28 15:52:37 +0000

    creating a model of bank

---
+++ poo/java_uml/banco/teste_banco.java
@@ -0,0 +1,26 @@
+class Teste_Banco {
+	public static void main(String[] args) {
+
+	}
+}
+
+public class Banco {
+
+}
+
+public class AtualizadorDeContas {
+
+}
+
+public class Conta {
+
+}
+
+public class ContaCorrente extends Conta {
+
+}
+
+public class ContaPoupanca extends Conta {
+
+}
+
